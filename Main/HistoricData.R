afab21cc633575c0dff461330802d4a3c29e987b
Rebelion <mitek_bobrovskij@inbox.ru>
2016-12-28 01:03:48 +0300

    Fixed Data feeder procedures

--- Main/HistoricData.R
+++ Main/HistoricData.R
@@ -1,10 +1,18 @@
-HistoricalTickData <- function(type, symbol, maxDataPoints = '0', dataDirection = '1', requestID = 'Base Request', dataPointsPerSend = '', 
+GetData <- function(requestString, quotesColumns)
+{
+	quotes <- writeLines(requestString, get('lookupConnection', envir = .GlobalEnv));
+	result <- rbindlist(lapply(res[c(-1, - length(res))], function(elem) { tstrsplit(elem, split = ',', fixed = TRUE); }));
+
+	setnames(result, paste("V", 1:ncol(result), sep = ""), quotesColumns);
+
+	result
+}
+
+HistoricTickData <- function(type, symbol, maxDataPoints = '0', dataDirection = '1', requestID = 'Base Request', dataPointsPerSend = '',
 							   beginFilterTime = '', endFilterTime = '', days = '', beginDate = '', endDate = '', beginTime = '',
 							   endTime = '')
 {
 	requestType <- paste('HT', type, sep = '');
-
-	baseRequestString <- paste(requestType, symbol, maxDataPoints, dataDirection, requestID, dataPointsPerSend, sep = ',');
 
 	requestString <- switch(type,
 							'X' = paste(requestType, symbol, maxDataPoints, dataDirection, requestID, dataPointsPerSend, sep = ','),
@@ -14,9 +22,89 @@ HistoricalTickData <- function(type, symbol, maxDataPoints = '0', dataDirection 
 										beginFilterTime, endFilterTime, dataDirection, requestID, dataPointsPerSend, sep = ','));
 
 	requestString <- paste(requestString, '\r\n', sep = '');
+	quotesColumns <- c('Request ID', 'TimeStamp', 'Last', 'Last Size', 'Total Volume', 'Bid', 'Ask', 'TickID', 'Basis For Last',
+					   'Trade Market Center', 'Trade Conditions');
 
-	quotes <- writeLines(requestString, get('lookupConnection', envir = .GlobalEnv));
-	result <- rbindlist(lapply(res[c(-1, - length(res))], function(elem) { tstrsplit(elem, split = ',', fixed = TRUE); }));
+	result <- GetData(requestString, quotesColumns);
+	result
+}
 
+HistoricIntervalData <- function(type, symbol, interval = '60', maxDataPoints = '0', dataDirection = '1', requestID = 'Base Request', 
+								 dataPointsPerSend = '', beginFilterTime = '', endFilterTime = '', days = '', beginDate = '', 
+								 endDate = '', beginTime = '', endTime = '') 
+{
+	requestType <- paste('HI', type, sep = '');
+
+	baseRequestString <- paste(requestType, symbol, maxDataPoints, dataDirection, requestID, dataPointsPerSend, sep = ',');
+
+	requestString <- switch(type,
+							'X' = paste(requestType, symbol, interval, maxDataPoints, dataDirection, requestID, dataPointsPerSend, sep = ','),
+							'D' = paste(requestType, symbol, interval, days, maxDataPoints, beginFilterTime, endFilterTime, dataDirection,
+										requestID, dataPointsPerSend, sep = ','),
+							'T' = paste(requestType, symbol, interval, paste(beginDate, beginTime, sep = ' '), paste(endDate, endTime, sep = ' '),
+										beginFilterTime, endFilterTime, dataDirection, requestID, dataPointsPerSend, sep = ','));
+
+	requestString <- paste(requestString, '\r\n', sep = '');
+	quotesColumns <- c('Request ID', 'TimeStamp', 'High', 'Low', 'Open', 'Close', 'Total Volume', 'Period Volume', 'Number of Trades');
+
+	result <- GetData(requestString, quotesColumns);
+	result
+}
+
+HistoricEODData <- function(type, symbol, maxDataPoints = '0', dataDirection = '1', requestID = 'Base Request',
+								 dataPointsPerSend = '', beginDate = '', endDate = '') 
+{
+	requestType <- paste('HD', type, sep = '');
+
+	requestString <- switch(type,
+							'X' = paste(requestType, symbol, maxDataPoints, dataDirection, requestID, dataPointsPerSend, sep = ','),
+							'T' = paste(requestType, symbol, beginDate, endDate, maxDataPoints, dataDirection, requestID, 
+										dataPointsPerSend, sep = ','));
+
+	requestString <- paste(requestString, '\r\n', sep = '');
+	quotesColumns <- c('Request ID', 'DateStamp', 'High', 'Low', 'Open', 'Close', 'Period Volume', 'Open Interest');
+
+	result <- GetData(requestString, quotesColumns);
+	result
+}
+
+HistoricEODData <- function(type, symbol, maxDataPoints = '0', dataDirection = '1', requestID = 'Base Request',
+								 dataPointsPerSend = '', beginDate = '', endDate = '') 
+{
+	requestType <- paste('HD', type, sep = '');
+
+	requestString <- switch(type,
+							'X' = paste(requestType, symbol, maxDataPoints, dataDirection, requestID, dataPointsPerSend, sep = ','),
+							'T' = paste(requestType, symbol, beginDate, endDate, maxDataPoints, dataDirection, requestID,
+										dataPointsPerSend, sep = ','));
+
+	requestString <- paste(requestString, '\r\n', sep = '');
+	quotesColumns <- c('Request ID', 'DateStamp', 'High', 'Low', 'Open', 'Close', 'Period Volume', 'Open Interest');
+
+	result <- GetData(requestString, quotesColumns);
+	result
+}
+
+HistoricEOWData <- function(symbol, maxDataPoints = '0', dataDirection = '1', requestID = 'Base Request',
+								 dataPointsPerSend = '', beginDate = '', endDate = '') 
+{
+	requestString <- paste('HWX', symbol, maxDataPoints, dataDirection, requestID, dataPointsPerSend, sep = ',');
+	requestString <- paste(requestString, '\r\n', sep = '');
+
+	quotesColumns <- c('Request ID', 'DateStamp', 'High', 'Low', 'Open', 'Close', 'Period Volume', 'Open Interest');
+
+	result <- GetData(requestString, quotesColumns);
+	result
+}
+
+HistoricEOMData <- function(symbol, maxDataPoints = '0', dataDirection = '1', requestID = 'Base Request',
+								 dataPointsPerSend = '', beginDate = '', endDate = '') 
+{
+	requestString <- paste('HMX', symbol, maxDataPoints, dataDirection, requestID, dataPointsPerSend, sep = ',');
+	requestString <- paste(requestString, '\r\n', sep = '');
+
+	quotesColumns <- c('Request ID', 'DateStamp', 'High', 'Low', 'Open', 'Close', 'Period Volume', 'Open Interest');
+
+	result <- GetData(requestString, quotesColumns);
 	result
 }
--- Main/Initialize.R
+++ Main/Initialize.R
@@ -1,8 +1,11 @@
-InitializeLookup <- function(lookup = 9100, protocol = '5.2') 
+
+InitializeLookup <- function(lookup = 9100, protocol = '5.2')
 {
-	lookupConnection <- socketConnection(host = "localhost", ports$lookup, server = FALSE, blocking = TRUE, open = "r+");
+	connection <- socketConnection(host = "localhost", ports$lookup, server = FALSE, blocking = TRUE, open = "r+");
 
 	protocolString <- paste('S,SET PROTOCOL,',protocol,'\r\n', sep = '')
-	writeLines(protocolString, lookupConnection);
+	writeLines(protocolString, connection);
+
+	assign(lookupConnection, connection, envir = .GloblEnv);
 }
 
